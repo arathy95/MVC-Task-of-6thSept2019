5fc06c35f1084de34a689ecb6da8f68c550ab186
Arathy Pillai <pillai.arathy95@hotmail.com>
2019-09-07 07:41:11 +0530

    Add project files.

---
+++ MVCTask2/Controllers/ImageController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MVCTask2.Controllers
+{
+    public class ImageController : Controller
+    {
+        // GET: Image
+        public ActionResult MainPage()
+        {
+            string imgpath = Server.MapPath("~/Content/Image/alphabet-tiranga-image-letter-name-tiranga-images-for-whatsapp-dp.jpg");
+            byte[] bytedata = System.IO.File.ReadAllBytes(imgpath);
+            string imgBase64Data = Convert.ToBase64String(bytedata);
+            string imgDataURL = string.Format("data:image/jpg;base64,{0}", imgBase64Data);
+            ViewBag.ImageData = imgDataURL;
+            return View();
+        }
+    }
+}
---
+++ MVCTask2/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MVCTask2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("MVCTask2")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("30d40db0-8802-4528-a714-b0242c1eb69c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
